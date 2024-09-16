0e6fdfd66e184fe48241a91214b733f24e5b3026
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-09-16 15:29:25 +0200

    Setup of Connectionstring to SqlServer Management Studio,
    Data folder with ApplicationDBContext created, setting up basestructure of Databases under way

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a42f7a4a0bbdec4ae8bec0fb5bbb302531989d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e1ea639922ea2fb85edb125030df277f32abf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
