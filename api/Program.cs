22360ab1a8a920f5dc73f58d438ec73269e8267f
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-09-17 14:07:07 +0200

    Video #8 Comment adding and adjustment and refactoring a bit Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -4,9 +4,9 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+// Swagger documentation is automatically added into this application, by adding the following services
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
