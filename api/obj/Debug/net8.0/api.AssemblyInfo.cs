38fe037802e7e00142c5836264c373e7528385e4
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-09-25 13:47:27 +0200

    Video #20 Pagination complete,
    Another query to QueryObject added, also added to the GetAll Method from Stocks(Thread)

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f491d186cbf906064a8e7e483b86a83f38a7d8a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54375edffcd24019f60e60046d5c87f21852efa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
