53d8570f440607fbadab676c0b6224e88fe79027
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-10-11 16:40:03 +0200

    REACT-S Video #14 Delete from portfolio done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,6 +25,14 @@ function App() {
     setPortfolioValues(updatedPortfolio);
   };
 
+  const onPortfolioDelete = (e: any) => {
+    e.preventDefault();
+    const removed = portfolioValues.filter((value) => {
+      return value !== e.target[0].value;
+    });
+    setPortfolioValues(removed);
+  };
+
   const onSearchSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
     const result = await searchCompanies(search);
@@ -39,7 +47,7 @@ function App() {
   return (
       <div className="App">
         <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange}/>
-        <ListPortfolio portfolioValues={portfolioValues}/>
+        <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete}/>
         <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
         {serverError && <h1>{serverError}</h1>}
     </div>
