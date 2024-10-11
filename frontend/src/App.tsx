f2b3d31f8c092990e5f8e2b68fdd50f8def71cf7
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-10-11 10:16:06 +0200

    REACT-S Video #8 done. General data flow and datastructure/logic set up, awesome!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,22 @@ import { ToastContainer } from 'react-toastify';
 import { UserProvider } from './Context/useAuth';
 import CardList from './Components/CardList/CardList';
 import Search from './Search/Search';
+import { ChangeEvent, SyntheticEvent, useState } from 'react';
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+
+  const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(e);
+  };
+
+  const onClick = (e: SyntheticEvent) => {
+    console.log(e);
+  };
   return (
       <div className="App">
-        <Search />
+        <Search onClick={onClick} search={search} handleChange={handleChange}/>
         <CardList />
     </div>
 
