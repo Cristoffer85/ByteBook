5b65595fd998f723c5bccb1710748f596af20c46
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-10-15 13:11:01 +0200

    REACT-S Video #19 First implementation of reusable tables in (Table.tsx) file and design-guide page (To house and show overall design guide implementations for the overall application)

---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Table from '../../Components/Table/Table'
+
+type Props = {}
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+        <>ByteBook Design Page</>
+        <h2>
+            This is ByteBooks Design Page. This is where we will house various design aspects for the application.
+        </h2>
+        <Table />
+    </>
+  )
+}
+
+export default DesignPage
