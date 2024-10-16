fde2c5b92e3f00762d8c4920a07f9cad1ac2f410
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-10-16 10:44:08 +0200

    REACT-S Video #21 Company Profile implemented

--- frontend/src/Pages/DesignPage/DesignPage.tsx
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -1,7 +1,15 @@
 import RatioList from '../../Components/RatioList/RatioList'
 import Table from '../../Components/Table/Table'
+import { testIncomeStatementData } from '../../Components/Table/testData'
 
 type Props = {}
+const tableConfig = [
+  {
+    label: "Market Cap",
+    render: (company: any) => company.marketCapTTM,
+    subTitle: "Total value of all a company's shares of stock",
+  }
+]
 
 const DesignPage = (props: Props) => {
   return (
@@ -10,7 +18,7 @@ const DesignPage = (props: Props) => {
         <h2>
             This is ByteBooks Design Page. This is where we will house various design aspects for the application.
         </h2>
-        <RatioList />
+        <RatioList data={testIncomeStatementData} config={tableConfig}/>
         <Table />
     </>
   )
